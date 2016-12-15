efb7f467137742f0e2ba3897f3e1986a3817a30f
J-Star <jvjasper53@gmail.com>
2016-12-15 18:32:37 +0100

    Multiple screen logic
    Added multiple screen logic

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+#!python3
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+class StartScreen(Screen):
+    pass
+
+class GameScreen(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+
+presentation = Builder.load_file("main.kv")
+
+class MainApp(App):
+    
+    def build(self):
+        return presentation
+
+if __name__ == "__main__":
+    MainApp().run()
